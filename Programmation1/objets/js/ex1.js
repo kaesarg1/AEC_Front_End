f7c64ff7806a878c8b796d7187df1cc8ae77b55b
kaesarg1 <franco.cesar@gmail.com>
2020-02-01 10:40:51 -0500

    test

---
+++ Programmation1/objets/js/ex1.js
@@ -0,0 +1,15 @@
+class Pokemon{
+
+    constructor(nom,type,hp,) {
+        this.nom = nom;
+        this.type = type;
+        this.hp = hp;
+        this.nomAttaque1 = nomAttqque1;
+        this.nomAttaque2 = nomAttaque2;
+        this.degatsAttaque1 = degatsAttaque1;
+        this.degatsAttaque2 = degatsAttaque2;
+    }
+
+}
+
+let pikachu = new pokemon ()
