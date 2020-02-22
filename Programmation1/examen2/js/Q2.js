1f8e85e08482391cd51eae34862801827e5e5435
kaesarg1 <franco.cesar@gmail.com>
2020-02-22 10:16:13 -0500

    jumbotron

---
+++ Programmation1/examen2/js/Q2.js
@@ -0,0 +1,18 @@
+/* Créé par Cesar Franco 02/15/2020*/
+
+
+let nb=1000;
+let lignes=10;
+let col=20;
+
+for(let i=0;i<lignes;i++){
+    for(let j=0;j<col;j++){
+        document.write(nb+" ");
+        nb-=5;
+    }
+    document.write("<br>");
+}
+
+
+
+
