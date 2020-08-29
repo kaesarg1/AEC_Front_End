3ff04d66820817400481d6130841de318e083033
kaesarg1 <franco.cesar@gmail.com>
2020-08-29 16:23:50 -0400

    rever to f32db9563d61c65814bc2fd7240cc8ef8e06c220

--- Integration1/Projet Final/js/service-worker.js
+++
@@ -1,9 +0,0 @@
-// Register service worker.
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('service-worker.js')
-            .then((reg) => {
-                console.log('Service worker registered.', reg);
-            });
-    });
-}
