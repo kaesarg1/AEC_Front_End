1223098de6ffbefc22f1d86509194d226acc11a3
kaesarg1 <franco.cesar@gmail.com>
2020-09-27 00:28:09 -0400

    Updates

---
+++ precache-manifest.5a5ef7d152dedcc92dfca401cec39ce0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5862a184ce369ee4da48b32420aec20c",
+    "url": "/AEC_Front_end/index.html"
+  },
+  {
+    "revision": "9788156df92e4ba64c08",
+    "url": "/AEC_Front_end/static/css/main.abefa447.chunk.css"
+  },
+  {
+    "revision": "d32c55f3a82e5dca61fe",
+    "url": "/AEC_Front_end/static/js/2.2eb32a02.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/AEC_Front_end/static/js/2.2eb32a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9788156df92e4ba64c08",
+    "url": "/AEC_Front_end/static/js/main.4a098ba4.chunk.js"
+  },
+  {
+    "revision": "d0cbb3a2519ff72e9ef2",
+    "url": "/AEC_Front_end/static/js/runtime-main.163d536f.js"
+  },
+  {
+    "revision": "78cfaca19713cd1a349a6cb120b05668",
+    "url": "/AEC_Front_end/static/media/debarras_logo_96x96.78cfaca1.ico"
+  }
+]);
