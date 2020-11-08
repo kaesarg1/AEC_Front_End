b09326c5b023e26eafc6c2732207cdd00b14fcb0
kaesarg1 <franco.cesar@gmail.com>
2020-11-08 17:19:42 -0500

    test

---
+++ portfolio/js/js.js
@@ -0,0 +1,10 @@
+function deferVideo() {
+
+    //defer html5 video loading
+    $("video source").each(function () {
+        var sourceFile = $(this).attr("data-src");
+        $(this).attr("src", sourceFile);
+        var video = this.parentElement;
+        video.load();
+    });
+}
